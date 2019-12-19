7b53ec776c3f8fc82b10df5c17fa09e1839099e6
ziyuan.li <ziyuan.li@ifchange.com>
2019-12-19 22:56:48 +0800

    feat: 修改微服务

--- src/lib/react-module-1/index.js
+++ src/lib/react-module-1/index.js
@@ -4,7 +4,7 @@ import ReactSPA from '../application/react-application';
 import sdk from '../sdk';
 
 class Module extends ReactSPA{
-  static dependencies = ['ifchange-plugin-components'];
+  dependencies = ['ifchange-plugin-components'];
 }
 
 sdk.registerModule('react-module-1', new Module());
--- src/lib/vue-module-1/index.js
+++ src/lib/vue-module-1/index.js
@@ -4,16 +4,17 @@ import VueSPA from '../application/vue-application';
 import sdk from '../sdk';
 
 class Module extends VueSPA{
-  static dependencies = ['ifchange-plugin-components'];
+  dependencies = ['ifchange-plugin-components'];
 }
 
+//https://cn.vuejs.org/v2/guide/components-dynamic-async.html
 sdk.registerModule('vue-module-1', new Module({
   data:{
     txt_name:""
   },
   //用于数据初始化
   created:function(){
-    this.txt_name = text;
+    // this.txt_name = text;
   },
   template: ``
 }));
